6efdb2c777ac850a2c55977c3e02bb8db71f0dd3
Luke McComb <lucasmccomb@gmail.com>
2014-09-23 11:03:20 -0600

    found container that has highest average temp

--- lib/metric_parser.rb
+++ lib/metric_parser.rb
@@ -9,6 +9,7 @@ class MetricParser
   def parse_container(container)
     parsed_file = CSV.read(@file_path, {:col_sep => "\t"})
     container_groups = parsed_file.group_by { |i| i[1] }
+    
     @container = container_groups[container]
 
     container_hash = {}
@@ -40,12 +41,23 @@ class MetricParser
 
     p container_hash
 
+  end
 
+  container_1 = self.new("data/metrics.tsv").parse_container("container1")
+  container_2 = self.new("data/metrics.tsv").parse_container("container2")
+  container_3 = self.new("data/metrics.tsv").parse_container("container3")
+
+  if container_1["average_temp"] > container_2["average_temp"] && container_1["average_temp"] > container_3["average_temp"]
+    p "#{container_1["container"]} has the highest average temperature."
+  elsif container_2["average_temp"] > container_1["average_temp"] && container_2["average_temp"] > container_3["average_temp"]
+    p "#{container_2["container"]} has the highest average temperature."
+  else
+    p "#{container_3["container"]} has the highest average temperature."
   end
 
 end
 
 
-MetricParser.new("data/metrics.tsv").parse_container("container1")
-MetricParser.new("data/metrics.tsv").parse_container("container2")
-MetricParser.new("data/metrics.tsv").parse_container("container3")
+# MetricParser.new("data/metrics.tsv").parse_container("container1")
+# MetricParser.new("data/metrics.tsv").parse_container("container2")
+# MetricParser.new("data/metrics.tsv").parse_container("container3")
